9d78eb9a89f9c4649f217494e3032e3b2cf14b91
Marcel017 <mcelogabriel17@gmail.com>
2025-12-06 16:02:36 -0300

    Estudando um pouco mais sobre JS

--- Integrando-Html-com-JS/main.js
+++ Integrando-Html-com-JS/main.js
@@ -1,12 +1,30 @@
 const form = document.getElementById("form-deposito");
 
 function validaNome(nomeCompleto){
-    const nomeComoArray = nomeCompleto.split(' ')
-    return nomeComoArray.length >= 2
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
 }
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-})
+form.addEventListener("submit", function(e){
+    let formValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente ${nomeBeneficiario.value} - Conta: ${numeroContaBeneficiario.value}`;
+
+    formValido = validaNome(nomeBeneficiario.value);
+    if (formValido) {
+        alert(mensagemSucesso); 
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
+    } else {
+        alert("O nome não está completo.");
+    }
+});
 
 console.log(form);
